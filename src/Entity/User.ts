f8d6cea0a1a7b34f28ddddce042adcaf61a9ad0a
이광훈 <leegh963@naver.com>
2024-03-24 15:19:08 +0900

    Feat : User Entity 설정

--- src/Entity/User.ts
+++ src/Entity/User.ts
@@ -1,7 +1,25 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
+    user_id: string;
+
+    @Column()
+    nickname: string;
+
+    @Column()
+    major: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    student_id: number;
+
+    @Column()
+    social_email: string;
 }
