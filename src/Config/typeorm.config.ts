f7aef300cf65eef0f3a18fd3d3f20e1a7c628fdb
koomin1227 <koomin1227@naver.com>
2024-03-25 16:54:04 +0900

    Feat : typeorm 설정시 환경변수 사용

--- src/Config/typeorm.config.ts
+++ src/Config/typeorm.config.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+    constructor(private configService: ConfigService) {}
+
     createTypeOrmOptions(): TypeOrmModuleOptions {
         return {
             type: 'mysql',
-            host: 'localhost',
-            port: 3306,
-            username: 'root',
-            password: 'root',
-            database: 'test',
+            host: this.configService.get('DB_HOST'),
+            port: this.configService.get<number>('DB_PORT'),
+            username: this.configService.get('DB_USERNAME'),
+            password: this.configService.get('DB_PASSWORD'),
+            database: this.configService.get('DB_DATABASE'),
             entities: [],
             synchronize: true,
         };
