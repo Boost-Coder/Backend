7aaef311b5f984b267fdac28442ed6bc34d4eea4
koomin1227 <koomin1227@naver.com>
2024-03-25 18:39:21 +0900

    Feat : 로그를 파일에 저장하는 기능 추가

--- src/Config/winston.config.ts
+++ src/Config/winston.config.ts
@@ -1,8 +1,10 @@
 import * as winston from 'winston';
 import {
+    utilities,
     utilities as nestWinstonModuleUtilities,
     WinstonModule,
 } from 'nest-winston';
+import * as winstonDaily from 'winston-daily-rotate-file';
 export const winstonOptions = new winston.transports.Console({
     level: process.env.NODE_ENV === 'prod' ? 'http' : 'silly',
     format: winston.format.combine(
@@ -14,6 +16,24 @@ export const winstonOptions = new winston.transports.Console({
     ),
 });
 
+export const dailyOption = () => {
+    return {
+        level: process.env.NODE_ENV === 'prod' ? 'info' : 'silly',
+        datePattern: 'YYYY-MM-DD',
+        dirname: `./logs/${process.env.NODE_ENV}`,
+        filename: `%DATE%.log`,
+        maxFiles: 30,
+        zippedArchive: true,
+        format: winston.format.combine(
+            winston.format.timestamp(),
+            utilities.format.nestLike('RankIn', {
+                colors: false,
+                prettyPrint: true,
+            }),
+        ),
+    };
+};
+
 export const winstonLogger = WinstonModule.createLogger({
-    transports: [winstonOptions],
+    transports: [winstonOptions, new winstonDaily(dailyOption())],
 });
