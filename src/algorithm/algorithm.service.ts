545fe9a7f3a65b1c28a302e7a2f5990b7f42265e
koomin1227 <koomin1227@naver.com>
2024-04-01 01:48:45 +0900

    Feat : solved ac의 정보를 얻어오는 기능 구현

--- src/algorithm/algorithm.service.ts
+++ src/algorithm/algorithm.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
+const URL = 'https://solved.ac/api/v3/user/show?handle=';
 @Injectable()
-export class AlgorithmService {}
+export class AlgorithmService {
+    async getBOJInfo(boj_id: string) {
+        try {
+            const res = await axios.get(URL + boj_id);
+            const boj_info = res.data;
+            return {
+                rating: boj_info.rating,
+                tier: boj_info.tier,
+                solvedCount: boj_info.solvedCount,
+            };
+        } catch (e) {
+            throw new Error('BOJ ID Not Found');
+        }
+    }
+}
