affc4d3a146ab43797b7d14d0b4dd8b2c06b8ba8
koomin1227 <koomin1227@naver.com>
2024-04-01 01:52:19 +0900

    Test : getBOJInfo 테스트 코드 작성

--- src/algorithm/algorithm.service.spec.ts
+++ src/algorithm/algorithm.service.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AlgorithmService } from './algorithm.service';
+import axios from 'axios';
 
+jest.mock('axios');
 describe('AlgorithmService', () => {
     let service: AlgorithmService;
 
@@ -14,5 +16,34 @@ describe('AlgorithmService', () => {
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+    });
+
+    describe('getBOJInfo', function () {
+        it('should return boj info', async function () {
+            const mockResponse = {
+                data: {
+                    rating: 1500,
+                    tier: 16,
+                    solvedCount: 100,
+                },
+            };
+            (axios.get as jest.Mock).mockResolvedValue(mockResponse);
+
+            const res = await service.getBOJInfo('userId');
+
+            expect(res.solvedCount).toEqual(100);
+            expect(res.rating).toEqual(1500);
+            expect(res.tier).toEqual(16);
+        });
+
+        it('should throw not found error', async function () {
+            (axios.get as jest.Mock).mockRejectedValue(
+                new Error('Failed to fetch'),
+            );
+
+            await expect(service.getBOJInfo('qwe')).rejects.toThrow(
+                'BOJ ID Not Found',
+            );
+        });
     });
 });
