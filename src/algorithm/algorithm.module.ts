c17186a145e4274852e482b78cb4e90c0960f1a4
koomin1227 <koomin1227@naver.com>
2024-04-01 01:00:02 +0900

    Feat : axios 등록

--- src/algorithm/algorithm.module.ts
+++ src/algorithm/algorithm.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AlgorithmController } from './algorithm.controller';
 import { AlgorithmService } from './algorithm.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+    imports: [HttpModule],
     controllers: [AlgorithmController],
     providers: [AlgorithmService],
 })
