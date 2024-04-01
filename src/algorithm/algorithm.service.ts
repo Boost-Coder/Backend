36860177606976aa7ac9bed05823546ba5b84be3
koomin1227 <koomin1227@naver.com>
2024-04-02 00:27:51 +0900

    Feat : 알고리즘 정보를 수정 하는 기능 구현

--- src/algorithm/algorithm.service.ts
+++ src/algorithm/algorithm.service.ts
@@ -51,6 +51,20 @@ export class AlgorithmService {
         await this.algorithmRepository.update(userId, algorithm);
     }
 
+    async modifyAlgorithm(userId: string, bojId: string) {
+        const algorithm = await this.algorithmRepository.findOneById(userId);
+        if (algorithm === null) {
+            throw new NotFoundError('Algorithm info not found');
+        }
+        const bojInfo = await this.getBOJInfo(bojId);
+        algorithm.bojId = bojId;
+        algorithm.tier = bojInfo.tier;
+        algorithm.rating = bojInfo.rating;
+        algorithm.solvedCount = bojInfo.solvedCount;
+        algorithm.point = this.calculatePoint(bojInfo);
+        await this.algorithmRepository.update(userId, algorithm);
+    }
+
     async getBOJInfo(bojId: string) {
         try {
             const res = await axios.get(URL + bojId);
