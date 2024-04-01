7672e0db2efe875f139aeecf5f9529ccb0d7b789
koomin1227 <koomin1227@naver.com>
2024-04-01 23:47:17 +0900

    Feat : algorithm 등록 컨트롤러 구현

--- src/algorithm/algorithm.controller.ts
+++ src/algorithm/algorithm.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AlgorithmService } from './algorithm.service';
+import { CreateAlgorithmDto } from './createAlgorithm.dto';
 
-@Controller('algorithm')
-export class AlgorithmController {}
+@Controller('api/stat/algorithm')
+export class AlgorithmController {
+    constructor(private readonly algorithmService: AlgorithmService) {}
+
+    @Post()
+    async algorithmCreate(@Body() body: CreateAlgorithmDto) {
+        const userId = 'user';
+        await this.algorithmService.createAlgorithm(userId, body.bojId);
+    }
+}
