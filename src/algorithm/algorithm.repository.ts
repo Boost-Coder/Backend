c1e0bf233d68ce892595c67a9eb05e2b06a428ef
koomin1227 <koomin1227@naver.com>
2024-04-01 22:48:10 +0900

    Feat : Algorithm save 구현

--- src/algorithm/algorithm.repository.ts
+++ src/algorithm/algorithm.repository.ts
@@ -1,11 +1,18 @@
 import { BaseRepository } from '../utils/base.repository';
 import { Inject, Injectable, Scope } from '@nestjs/common';
-import { DataSource } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { REQUEST } from '@nestjs/core';
+import { Algorithm } from '../Entity/algorithm';
 
 @Injectable({ scope: Scope.REQUEST })
 export class AlgorithmRepository extends BaseRepository {
+    private repository: Repository<Algorithm>;
     constructor(dataSource: DataSource, @Inject(REQUEST) req: Request) {
         super(dataSource, req);
+        this.repository = this.getRepository(Algorithm);
+    }
+
+    async save(algorithm: Algorithm) {
+        await this.repository.save(algorithm);
     }
 }
