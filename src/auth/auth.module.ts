4b92f7e45578e058d0482b6fc47ae8932613031d
koomin1227 <koomin1227@naver.com>
2024-04-07 23:11:35 +0900

    Feat: jwt 모듈 등록

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtConfig } from '../Config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+    imports: [JwtModule.registerAsync({ useClass: JwtConfig })],
     controllers: [AuthController],
     providers: [AuthService],
 })
