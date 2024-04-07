19e3efa124ecd511567f200a9b6a78a21883f9af
koomin1227 <koomin1227@naver.com>
2024-04-07 23:50:59 +0900

    Test: generateToken 테스트

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { AlgorithmRepository } from '../algorithm/algorithm.repository';
+import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import mock = jest.mock;
+import { User } from '../Entity/user';
+
+const mockUserService = {
+    getUserByProviderId: jest.fn(),
+    createUser: jest.fn(),
+};
+
+const mockJwtService = {
+    sign: jest.fn(),
+};
+
+const mockConfigService = {
+    get: jest.fn(),
+};
 
 describe('AuthService', () => {
     let service: AuthService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [AuthService],
+            providers: [
+                AuthService,
+                {
+                    provide: UserService,
+                    useValue: mockUserService,
+                },
+                {
+                    provide: JwtService,
+                    useValue: mockJwtService,
+                },
+                {
+                    provide: ConfigService,
+                    useValue: mockConfigService,
+                },
+            ],
         }).compile();
 
         service = module.get<AuthService>(AuthService);
@@ -14,5 +47,19 @@ describe('AuthService', () => {
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+    });
+
+    describe('generateToken', function () {
+        const user = new User();
+        user.userId = 'user';
+        it('should generate access token', function () {
+            mockJwtService.sign.mockResolvedValue('jwt-token');
+
+            service.generateAccessToken(user);
+
+            expect(mockJwtService.sign).toHaveBeenCalledWith({
+                userId: user.userId,
+            });
+        });
     });
 });
