fc2844609d9571fbb8e5c0aa6c477572ae440e5f
koomin1227 <koomin1227@naver.com>
2024-04-07 23:24:58 +0900

    Feat: 애플 로그인 컨트롤러 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
 import { AppleLoginDto } from './appleLogin.dto';
+import { AuthService } from './auth.service';
 
 @Controller('api')
 export class AuthController {
+    constructor(private authService: AuthService) {}
     @Post('appleOAuth')
-    signInWithApple(@Body() body: AppleLoginDto) {}
+    async signInWithApple(@Body() body: AppleLoginDto) {
+        const providerId = await this.authService.validateAppleOAuth(body);
+        if (!providerId) {
+            throw new UnauthorizedException('토큰이 유효하지 않음');
+        }
+        return this.authService.logInOrSignUp(providerId);
+    }
 }
