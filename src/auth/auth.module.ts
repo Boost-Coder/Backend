169b82ef1ab0efb4933f9857ae0ecded36f31274
koomin1227 <koomin1227@naver.com>
2024-04-07 23:28:33 +0900

    Feat: auth 모듈 설정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtConfig } from '../Config/jwt.config';
 import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from '../user/user.module';
 
 @Module({
-    imports: [JwtModule.registerAsync({ useClass: JwtConfig })],
+    imports: [JwtModule.registerAsync({ useClass: JwtConfig }), UserModule],
     controllers: [AuthController],
     providers: [AuthService],
 })
