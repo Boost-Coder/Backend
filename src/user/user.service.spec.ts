639159bf52bf47bada746f39d55faf78239d032e
koomin1227 <koomin1227@naver.com>
2024-04-08 00:25:16 +0900

    Test: generateUserId 테스트

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { JwtService } from '@nestjs/jwt';
+import { UserRepository } from './user.repository';
+
+const mockUserRepository = {
+    findOneByProviderId: jest.fn(),
+    save: jest.fn(),
+};
 
 describe('UserService', () => {
     let service: UserService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [UserService],
+            providers: [
+                UserService,
+                {
+                    provide: UserRepository,
+                    useValue: mockUserRepository,
+                },
+            ],
         }).compile();
 
         service = module.get<UserService>(UserService);
@@ -14,5 +27,13 @@ describe('UserService', () => {
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+    });
+
+    describe('generateUserId', function () {
+        it('should return userId of length 8.', function () {
+            const userId = service.generateUserId();
+
+            expect(userId.length).toEqual(8);
+        });
     });
 });
