cae4c353654c01b6e70147c3cd497592caace547
koomin1227 <koomin1227@naver.com>
2024-04-07 16:59:35 +0900

    Feat: auth module 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './Config/typeorm.config';
 import { ConfigModule } from '@nestjs/config';
 import { AlgorithmModule } from './algorithm/algorithm.module';
+import { AuthModule } from './auth/auth.module';
 import * as process from 'process';
 
 @Module({
@@ -17,6 +18,7 @@ import * as process from 'process';
             envFilePath: `${process.cwd()}/envs/${process.env.NODE_ENV}.env`,
         }),
         AlgorithmModule,
+        AuthModule,
     ],
     controllers: [AppController],
     providers: [AppService],
