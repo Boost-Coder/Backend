ce49bde6e2b06ef805522d7a3249ce48a4ef4d78
koomin1227 <koomin1227@naver.com>
2024-04-09 02:12:48 +0900

    Feat: 유저 정보를 업데이트 하는 컨트롤러 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Put } from '@nestjs/common';
+import { UpdateUserInfoDto } from './dto/update-user-info.dto';
+import { UserService } from './user.service';
 
 @Controller('api/users')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService) {}
+
+    @Put(':id')
+    async userModify(
+        @Body() body: UpdateUserInfoDto,
+        @Param('id') userId: string,
+    ) {
+        await this.userService.updateUserInfo(userId, body);
+    }
+}
