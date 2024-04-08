e94f17f947a27d4c0e9adfd0e22721b76bc8e323
koomin1227 <koomin1227@naver.com>
2024-04-09 00:14:33 +0900

    Feat: algorithmController 에 가드 적용

--- src/algorithm/algorithm.controller.ts
+++ src/algorithm/algorithm.controller.ts
@@ -1,14 +1,24 @@
-import { Body, Controller, Delete, Param, Patch, Post } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Param,
+    Patch,
+    Post,
+    UseGuards,
+} from '@nestjs/common';
 import { AlgorithmService } from './algorithm.service';
 import { CreateAlgorithmDto } from './createAlgorithm.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { UserId } from '../decorator/user-id.decorator';
 
+@UseGuards(JwtAuthGuard)
 @Controller('api/stat/algorithm')
 export class AlgorithmController {
     constructor(private readonly algorithmService: AlgorithmService) {}
 
     @Post()
-    async algorithmCreate(@Body() body: CreateAlgorithmDto) {
-        const userId = 'user';
+    async algorithmCreate(@Body() body: CreateAlgorithmDto, @UserId() userId) {
         await this.algorithmService.createAlgorithm(userId, body.bojId);
     }
 
