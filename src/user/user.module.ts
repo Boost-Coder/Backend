eddc334c9fd46039120a19394a817c3c07231a85
koomin1227 <koomin1227@naver.com>
2024-04-09 01:19:19 +0900

    Feat: UserController 등록

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserRepository } from './user.repository';
+import { UserController } from './user.controller';
 
 @Module({
+    controllers: [UserController],
     providers: [UserService, UserRepository],
     exports: [UserService],
 })
