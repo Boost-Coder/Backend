b141b5e13608c763422e8d434bc2bc7657809dda
koomin1227 <koomin1227@naver.com>
2024-04-09 02:15:46 +0900

    Feat: 인증, 인가된 사용자만 user 정보를 수정 하도록 수정

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,15 @@
-import { Body, Controller, Param, Put } from '@nestjs/common';
+import { Body, Controller, Param, Put, UseGuards } from '@nestjs/common';
 import { UpdateUserInfoDto } from './dto/update-user-info.dto';
 import { UserService } from './user.service';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
+import { OwnershipGuard } from '../auth/guard/ownership.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('api/users')
 export class UserController {
     constructor(private userService: UserService) {}
 
+    @UseGuards(OwnershipGuard)
     @Put(':id')
     async userModify(
         @Body() body: UpdateUserInfoDto,
