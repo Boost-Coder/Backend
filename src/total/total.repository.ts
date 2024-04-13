2310136898f9ddc85a74c41b93a5a43292dd3dcc
koomin1227 <koomin1227@naver.com>
2024-04-13 21:56:38 +0900

    Feat: TotalPoint update 메서드 구현

---
+++ src/total/total.repository.ts
@@ -0,0 +1,19 @@
+import { BaseRepository } from '../utils/base.repository';
+import { DataSource, Repository } from 'typeorm';
+import { Grade } from '../Entity/grade';
+import { Inject } from '@nestjs/common';
+import { REQUEST } from '@nestjs/core';
+import { TotalPoint } from '../Entity/totalPoint';
+
+export class TotalRepository extends BaseRepository {
+    private repository: Repository<Grade>;
+
+    constructor(dataSource: DataSource, @Inject(REQUEST) req: Request) {
+        super(dataSource, req);
+        this.repository = this.getRepository(Grade);
+    }
+
+    async update(total: TotalPoint, userId: string) {
+        return await this.repository.update({ userId: userId }, total);
+    }
+}
