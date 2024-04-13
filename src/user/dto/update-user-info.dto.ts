c71044701e6c2d14b8ecc107634e3c147a8aa37c
이광훈 <leegh963@naver.com>
2024-04-13 18:31:29 +0900

    Feat : userUpdateDto 복원

--- src/user/dto/update-user-info.dto.ts
+++ src/user/dto/update-user-info.dto.ts
@@ -1,6 +1,18 @@
-import { IsString } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 export class UpdateUserInfoDto {
     @IsString()
     nickname: string;
+
+    @IsString()
+    major: string;
+
+    @IsString()
+    name: string;
+
+    @IsNumber()
+    studentId: number;
+
+    @IsString()
+    birthDate: Date;
 }
