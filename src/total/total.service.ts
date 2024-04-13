18fd01c5b4431ba29920838e19b4be5f03ce8141
koomin1227 <koomin1227@naver.com>
2024-04-14 02:38:03 +0900

    Feat: 종합 점수를 업데이트 하는 함수 구현

--- src/total/total.service.ts
+++ src/total/total.service.ts
@@ -1,12 +1,23 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { TotalRepository } from './total.repository';
 import { TotalPoint } from '../Entity/totalPoint';
+import { GithubService } from '../github/github.service';
+import { AlgorithmService } from '../algorithm/algorithm.service';
+import { GradeService } from '../grade/grade.service';
+import { Github } from '../Entity/github';
+import { Grade } from '../Entity/grade';
+import { Algorithm } from '../Entity/algorithm';
 
 @Injectable()
 export class TotalService {
-    constructor(private totalRepository: TotalRepository) {}
+    constructor(
+        private totalRepository: TotalRepository,
+        private githubService: GithubService,
+        private algorithmService: AlgorithmService,
+        private gradeService: GradeService,
+    ) {}
     async createTotalPoint(userId: string) {
-        const isExist = await this.totalRepository.findOne(userId);
+        const isExist = await this.totalRepository.findOneById(userId);
 
         if (isExist) {
             throw new BadRequestException('이미 등록했습니다.');
@@ -16,5 +27,18 @@ export class TotalService {
         totalPoint.point = 0;
 
         await this.totalRepository.save(totalPoint);
+    }
+
+    async updateTotal(userId) {
+        const github = await this.githubService.findGithub(userId);
+        const algorithm = await this.algorithmService.findAlgorithm(userId);
+        const grade = await this.gradeService.findGrade(userId);
+        const total = new TotalPoint();
+        total.point = this.calculateTotalPoint(github, algorithm, grade);
+        await this.totalRepository.update(total, userId);
+    }
+
+    calculateTotalPoint(github: Github, algorithm: Algorithm, grade: Grade) {
+        return 0;
     }
 }
