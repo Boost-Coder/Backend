3b8efcbbecc20693181662d5d7b0af48d05e4b1f
koomin1227 <koomin1227@naver.com>
2024-04-14 02:37:06 +0900

    Test: createTotalPoint 테스트

--- src/total/total.service.spec.ts
+++ src/total/total.service.spec.ts
@@ -1,18 +1,76 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TotalService } from './total.service';
+import { TotalRepository } from './total.repository';
+import { User } from '../Entity/user';
+import { GithubService } from '../github/github.service';
+import { GradeService } from '../grade/grade.service';
+import { AlgorithmService } from '../algorithm/algorithm.service';
 
+const mockTotalRepository = {
+    save: jest.fn(),
+    findOneById: jest.fn(),
+    update: jest.fn(),
+};
+
+const mockGitService = {
+    findGithub: jest.fn(),
+};
+
+const mockGradeService = {
+    findGrade: jest.fn(),
+};
+
+const mockAlgorithmService = {
+    findAlgorithm: jest.fn(),
+};
 describe('TotalService', () => {
     let service: TotalService;
+    let repository;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [TotalService],
+            providers: [
+                TotalService,
+                { provide: TotalRepository, useValue: mockTotalRepository },
+                {
+                    provide: GithubService,
+                    useValue: mockGitService,
+                },
+                {
+                    provide: GradeService,
+                    useValue: mockGradeService,
+                },
+                {
+                    provide: AlgorithmService,
+                    useValue: mockAlgorithmService,
+                },
+            ],
         }).compile();
 
         service = module.get<TotalService>(TotalService);
+        repository = module.get(TotalRepository);
     });
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+    });
+
+    describe('createTotalPoint', function () {
+        it('should create TotalPoint', async function () {
+            const userId = 'user';
+            repository.findOneById.mockResolvedValue(null);
+
+            await service.createTotalPoint(userId);
+
+            expect(repository.save).toHaveBeenCalled();
+        });
+
+        it('중복 등록시 에러', function () {
+            const userId = 'user';
+            repository.findOneById.mockResolvedValue(new User());
+            expect(service.createTotalPoint(userId)).rejects.toThrow(
+                '이미 등록했습니다.',
+            );
+        });
     });
 });
