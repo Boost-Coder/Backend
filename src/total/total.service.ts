8b3727569fe76be5f573b3826c0501de162c3fea
koomin1227 <koomin1227@naver.com>
2024-04-13 22:05:07 +0900

    Feat: TotalPoint 생성하는 기능 구현

--- src/total/total.service.ts
+++ src/total/total.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { TotalRepository } from './total.repository';
+import { TotalPoint } from '../Entity/totalPoint';
 
 @Injectable()
-export class TotalService {}
+export class TotalService {
+    constructor(private totalRepository: TotalRepository) {}
+    async createTotalPoint(userId: string) {
+        const isExist = await this.totalRepository.findOne(userId);
+
+        if (isExist) {
+            throw new BadRequestException('이미 등록했습니다.');
+        }
+        const totalPoint = new TotalPoint();
+        totalPoint.userId = userId;
+        totalPoint.point = 0;
+
+        await this.totalRepository.save(totalPoint);
+    }
+}
