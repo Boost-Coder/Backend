fdcac0a30073fd1de818c02b06801d68cd98e1fd
koomin1227 <koomin1227@naver.com>
2024-04-14 09:58:13 +0900

    Feat: 회원가입 시 totalPoint 생성

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,14 @@ import { JwtConfig } from '../Config/jwt.config';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '../user/user.module';
 import { JwtStrategy } from './strategy/jwt.strategy';
+import { TotalModule } from '../total/total.module';
 
 @Module({
-    imports: [JwtModule.registerAsync({ useClass: JwtConfig }), UserModule],
+    imports: [
+        JwtModule.registerAsync({ useClass: JwtConfig }),
+        UserModule,
+        TotalModule,
+    ],
     controllers: [AuthController],
     providers: [AuthService, JwtStrategy],
 })
