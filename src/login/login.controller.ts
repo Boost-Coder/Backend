90f6545251bd7d7c038c5b9ba08be46e1da527ce
이광훈 <leegh963@naver.com>
2024-04-14 16:15:28 +0900

    Feat : controller 작성

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,8 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
 import { LoginService } from './login.service';
 
 @Controller('login')
 export class LoginController {
     constructor(private loginService: LoginService) {}
-    public refreshTokens() {}
+    public refreshTokens(@Body('refreshToken') refreshToken: string) {
+        return this.loginService.sendTokens(refreshToken);
+    }
 }
