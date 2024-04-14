aaf80b04bbdbcd2b055590fb71997f2c4f7f90f1
이광훈 <leegh963@naver.com>
2024-04-14 16:34:42 +0900

    Fix: url 매칭 설정

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { LoginService } from './login.service';
 
-@Controller('login')
+@Controller('/api')
 export class LoginController {
     constructor(private loginService: LoginService) {}
+
+    @Post('login/refresh')
     public refreshTokens(@Body('refreshToken') refreshToken: string) {
         return this.loginService.sendTokens(refreshToken);
     }
