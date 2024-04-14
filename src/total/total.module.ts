240c8c8f2671611eb27aa8ad3b24da066192577d
koomin1227 <koomin1227@naver.com>
2024-04-14 09:57:35 +0900

    Feat: Total module 등록

--- src/total/total.module.ts
+++ src/total/total.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TotalService } from './total.service';
+import { TotalRepository } from './total.repository';
+import { GithubModule } from '../github/github.module';
+import { AlgorithmModule } from '../algorithm/algorithm.module';
+import { GradeModule } from '../grade/grade.module';
 
 @Module({
-  providers: [TotalService]
+    imports: [GithubModule, AlgorithmModule, GradeModule],
+    providers: [TotalService, TotalRepository],
+    exports: [TotalService],
 })
 export class TotalModule {}
