f7d06f9b64ff9082e9dd34f665044def0c69fa4a
koomin1227 <koomin1227@naver.com>
2024-04-17 03:57:06 +0900

    Feat: algorithm 데이터에 rank 를 붙여서 조회하는 메서드 구현

--- src/stat/repository/algorithm.repository.ts
+++ src/stat/repository/algorithm.repository.ts
@@ -3,6 +3,8 @@ import { Inject, Injectable, Scope } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { REQUEST } from '@nestjs/core';
 import { Algorithm } from '../../Entity/algorithm';
+import { RankListOptionDto } from '../../rank/rank-list-option.dto';
+import { User } from '../../Entity/user';
 
 @Injectable({ scope: Scope.REQUEST })
 export class AlgorithmRepository extends BaseRepository {
@@ -18,6 +20,32 @@ export class AlgorithmRepository extends BaseRepository {
 
     async findOneById(userId: string) {
         return await this.repository.findOneBy({ userId: userId });
+    }
+
+    async findAlgorithmWithRank(options: RankListOptionDto) {
+        const queryBuilder = this.repository
+            .createQueryBuilder()
+            .select(['b.rank', 'b.user_id', 'b.point'])
+            .distinct(true)
+            .from((sub) => {
+                return sub
+                    .select('RANK() OVER (ORDER BY a.point DESC)', 'rank')
+                    .addSelect('a.user_id', 'user_id')
+                    .addSelect('a.point', 'point')
+                    .from(Algorithm, 'a');
+            }, 'b')
+            .innerJoin(User, 'u', 'b.user_id = u.user_id')
+            .addSelect('u.nickname', 'nickname')
+            .where('b.point < :point', { point: options.cursorPoint })
+            .orWhere('b.point = :point AND b.user_id > :userId', {
+                point: options.cursorPoint,
+                userId: options.cursorUserId,
+            })
+            .orderBy('point', 'DESC')
+            .addOrderBy('user_id')
+            .limit(3);
+
+        return await queryBuilder.getRawMany();
     }
 
     async update(userId: string, algorithm: Algorithm) {
