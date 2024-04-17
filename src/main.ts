feb179f071d8888f207fc3e894915725b4f9393c
koomin1227 <koomin1227@naver.com>
2024-04-18 00:02:23 +0900

    Feat: swagger 문서 설정

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { winstonLogger } from './Config/winston.config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         logger: winstonLogger,
     });
+    const config = new DocumentBuilder()
+        .setTitle('Rank In')
+        .setDescription('RankIn API Document')
+        .setVersion('1.0')
+        .addTag('auth')
+        .addTag('users')
+        .addTag('stat')
+        .addTag('rank')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
     app.useGlobalPipes(new ValidationPipe());
     await app.listen(3000);
 }
