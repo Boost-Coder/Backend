0b5dd6080db65c3502f2d3181794be4f9cf699c5
koomin1227 <koomin1227@naver.com>
2024-04-17 23:20:59 +0900

    Feat: findAlgorithmRank 컨트롤러 구현

--- src/rank/rank.controller.ts
+++ src/rank/rank.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 import { RankListOptionDto } from './rank-list-option.dto';
+import { AlgorithmService } from '../stat/service/algorithm.service';
 
 @UseGuards(JwtAuthGuard)
 @Controller('api/rank')
 export class RankController {
+    constructor(private algorithmService: AlgorithmService) {}
     @Get('algorithm')
-    findAll(@Query() options: RankListOptionDto) {
-        return {};
+    async findAlgorithmRank(@Query() options: RankListOptionDto) {
+        return await this.algorithmService.getAlgorithms(options);
     }
 }
