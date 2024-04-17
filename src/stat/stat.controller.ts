1ff34ad19ef33c009885628557a90a8ce6dc6ecd
koomin1227 <koomin1227@naver.com>
2024-04-18 01:54:49 +0900

    Feat: Post /api/stat/grade swagger 문서 추가

--- src/stat/stat.controller.ts
+++ src/stat/stat.controller.ts
@@ -21,6 +21,7 @@ import { TotalService } from './service/total.service';
 import {
     ApiBadRequestResponse,
     ApiBearerAuth,
+    ApiBody,
     ApiConflictResponse,
     ApiCreatedResponse,
     ApiForbiddenResponse,
@@ -150,6 +151,40 @@ export class StatController {
     }
 
     @ApiTags('stat')
+    @ApiOperation({
+        summary: '학점 역량 등록 API',
+        description: '학점을 등록한다. 이미 등록된 경우는 등록되지 않는다. ',
+    })
+    @ApiBearerAuth('accessToken')
+    @ApiCreatedResponse({
+        description: '학점 역량 등록 성공',
+    })
+    @ApiUnauthorizedResponse({
+        description: 'jwt 관련 문제 (인증 시간이 만료됨, jwt를 보내지 않음)',
+    })
+    @ApiForbiddenResponse({
+        description: '허용되지 않은 자원에 접근한 경우. 즉, 권한이 없는 경우',
+    })
+    @ApiNotFoundResponse({
+        description:
+            'user가 존재하지 않는 경우. 즉, 작업하려는 user가 존재하지 않는 경우',
+    })
+    @ApiConflictResponse({
+        description: '이미 등록한 경우',
+    })
+    @ApiInternalServerErrorResponse({
+        description: '서버 오류',
+    })
+    @ApiBody({
+        schema: {
+            type: 'object',
+            properties: {
+                grade: {
+                    type: 'number',
+                },
+            },
+        },
+    })
     @Post('grade')
     public async createGrade(
         @Body('grade') grade: number,
