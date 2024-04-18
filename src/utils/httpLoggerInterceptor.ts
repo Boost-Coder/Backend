582cc56634795df01f119b98fe3f3d8148c0eca3
이광훈 <leegh963@naver.com>
2024-04-18 17:52:50 +0900

    Feat : interceptor 를 이용한 로깅 처리

---
+++ src/utils/httpLoggerInterceptor.ts
@@ -0,0 +1,40 @@
+import {
+    CallHandler,
+    ExecutionContext,
+    NestInterceptor,
+    Injectable,
+    Logger,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
+import { Request } from 'express';
+
+@Injectable()
+export class HttpLoggerInterceptor implements NestInterceptor {
+    private readonly logger = new Logger();
+    intercept(
+        context: ExecutionContext,
+        next: CallHandler<any>,
+    ): Observable<any> | Promise<Observable<any>> {
+        const request: Request = context.switchToHttp().getRequest();
+        this.logger.debug(request.body, 'HTTP');
+
+        return next.handle().pipe(
+            tap((data) => {
+                const request = context.switchToHttp().getRequest();
+                const response = context.switchToHttp().getResponse();
+                this.logger.debug(
+                    `<${request.userId}> ${request.method} ${request.url} ${response.statusCode} ${response.statusMessage}`,
+                    'HTTP',
+                );
+            }),
+            catchError((err) => {
+                this.logger.error(
+                    `<${request['userId']}> ${request.method} ${request.url}  [Error]${err}`,
+                    'HTTP ERROR',
+                );
+                return throwError(err);
+            }),
+        );
+    }
+}
