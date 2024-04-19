e84b54e0d31d3fb5feaf3c0bb441c28a19d2394d
koomin1227 <koomin1227@naver.com>
2024-04-19 22:33:28 +0900

    Feat: 깃허브 랭크를 조회하는 컨트롤러 구현

--- src/stat/rank.controller.ts
+++ src/stat/rank.controller.ts
@@ -52,6 +52,31 @@ export class RankController {
         return await this.algorithmService.getAlgorithms(options);
     }
 
+    @Get('github')
+    @ApiTags('rank')
+    @ApiOperation({
+        summary: '깃허브 전체 랭킹 API',
+        description:
+            '깃허브 역량의 랭킹리스트를 반환한다. 페이지네이션이 가능하고 학과 별로 필터링 가능하다. (학번 필터링은 아직 미구현) 커서 사용시 cursorPoint, cursorUserId 두개를 동시에 넣어야한다. 각각 마지막으로 받은 유저의 점수와 유저 아이디이다.',
+    })
+    @ApiBearerAuth('accessToken')
+    @ApiOkResponse({
+        description: '깃허브 랭킹 반환',
+        type: [RankListDto],
+    })
+    @ApiUnauthorizedResponse({
+        description: 'jwt 관련 문제 (인증 시간이 만료됨, jwt를 보내지 않음)',
+    })
+    @ApiForbiddenResponse({
+        description: '허용되지 않은 자원에 접근한 경우. 즉, 권한이 없는 경우',
+    })
+    @ApiInternalServerErrorResponse({
+        description: '서버 오류',
+    })
+    async findGithubRank(@Query() options: RankListOptionDto) {
+        return await this.githubService.getGithubRank(options);
+    }
+
     @Get('/users/:id')
     @ApiTags('rank')
     @ApiOperation({
