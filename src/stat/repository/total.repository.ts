efcad5f81f1124126cbb108772274e75d683b8a2
koomin1227 <koomin1227@naver.com>
2024-04-19 22:55:42 +0900

    Feat: 종합 랭크를 쿼리하는 메서드 구현

--- src/stat/repository/total.repository.ts
+++ src/stat/repository/total.repository.ts
@@ -4,10 +4,11 @@ import { Grade } from '../../Entity/grade';
 import { Inject } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
 import { TotalPoint } from '../../Entity/totalPoint';
-import { RankListOptionDto } from '../dto/rank-list-option.dto';
+import { RankListDto, RankListOptionDto } from '../dto/rank-list-option.dto';
 import { Algorithm } from '../../Entity/algorithm';
 import { User } from '../../Entity/user';
 import { PointFindDto } from '../dto/rank-find.dto';
+import { Github } from '../../Entity/github';
 
 export class TotalRepository extends BaseRepository {
     private repository: Repository<TotalPoint>;
@@ -57,6 +58,38 @@ export class TotalRepository extends BaseRepository {
             return `u.major like '${options.major}'`;
         } else {
             return `u.id > 0`;
+        }
+    }
+
+    async findTotalWithRank(
+        options: RankListOptionDto,
+    ): Promise<[RankListDto]> {
+        const queryBuilder = this.repository
+            .createQueryBuilder()
+            .select(['b.rank', 'b.user_id', 'b.point', 'b.nickname'])
+            .distinct(true)
+            .from((sub) => {
+                return sub
+                    .select('RANK() OVER (ORDER BY a.point DESC)', 'rank')
+                    .addSelect('a.user_id', 'user_id')
+                    .addSelect('a.point', 'point')
+                    .addSelect('u.nickname', 'nickname')
+                    .from(TotalPoint, 'a')
+                    .innerJoin(User, 'u', 'a.user_id = u.user_id')
+                    .where(this.createClassificationOption(options));
+            }, 'b')
+            .where(this.createCursorOption(options))
+            .orderBy('point', 'DESC')
+            .addOrderBy('user_id')
+            .limit(3);
+        return await (<Promise<[RankListDto]>>queryBuilder.getRawMany());
+    }
+
+    createCursorOption(options: RankListOptionDto) {
+        if (!options.cursorPoint && !options.cursorUserId) {
+            return 'b.point > -1';
+        } else {
+            return `b.point < ${options.cursorPoint} or b.point = ${options.cursorPoint} AND b.user_id > '${options.cursorUserId}'`;
         }
     }
 }
