2c20a88e8a36f30a9b668719a6c76528b8447636
koomin1227 <koomin1227@naver.com>
2024-04-22 17:37:07 +0900

    Feat: 알고리즘 역량이 없는경우 업데이트 하지 않도록 수정 및 에러 메시지 출력

--- src/stat/service/algorithm.service.ts
+++ src/stat/service/algorithm.service.ts
@@ -56,7 +56,7 @@ export class AlgorithmService {
     async updateAlgorithm(userId: string) {
         const algorithm = await this.algorithmRepository.findOneById(userId);
         if (algorithm === null) {
-            throw new NotFoundError('Algorithm info not found');
+            return;
         }
         try {
             const bojInfo = await this.getBOJInfo(algorithm.bojId);
@@ -70,7 +70,9 @@ export class AlgorithmService {
                 await this.removeAlgorithm(userId);
                 this.logger.warn(`${userId} 님의 알고리즘 스탯이 초기화됨.`);
             } else {
-                throw e;
+                this.logger.error(
+                    `${userId} 님의 알고리즘 스탯이 업데이트 되지 않음. ${e}`,
+                );
             }
         }
     }
